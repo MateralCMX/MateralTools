1afcc1437b9cc74e2d7d9fc3dd38ea5580b98b53
陈明旭 <cloomcmx1554@hotmail.com>
2017-08-25 17:20:23 +0800

    完成关键字过滤库

---
+++ MateralTools.MDirtyWord/Interface/IDirtWordManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MateralTools.MDirtyWord
+{
+    /// <summary>
+    /// 关键词管理器接口
+    /// </summary>
+    public interface IKeyWordManager
+    {
+        /// <summary>
+        /// 脏字列表
+        /// </summary>
+        string[] Keywords { get; set; }
+        /// <summary>
+        /// 搜索所有的关键词
+        /// </summary>
+        /// <param name="text">要搜索的文本</param>
+        /// <returns>搜索到的对象</returns>
+        DirtyWordModel[] FindAll(string text);
+        /// <summary>
+        /// 搜索第一个关键词
+        /// </summary>
+        /// <param name="text">要搜索的文本</param>
+        /// <returns>搜索到的对象</returns>
+        DirtyWordModel FindFirst(string text);
+        /// <summary>
+        /// 是否包含
+        /// </summary>
+        /// <param name="text">要搜索的文本</param>
+        /// <returns>是否包含关键词</returns>
+        bool ContainsAny(string text);
+    }
+}
---
+++ MateralTools.MDirtyWord/Model/DirtyWordModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MateralTools.MDirtyWord
+{
+    public class DirtyWordModel
+    {
+        /// <summary>
+        /// 索引位置
+        /// </summary>
+        private int _index;
+        /// <summary>
+        /// 脏字
+        /// </summary>
+        private string _keyword;
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="index">文本索引位置</param>
+        /// <param name="keyword">找到的文本</param>
+        public DirtyWordModel(int index, string keyword)
+        {
+            _index = index;
+            _keyword = keyword;
+        }
+        /// <summary>
+        /// 索引位置
+        /// </summary>
+        public int Index
+        {
+            get { return _index; }
+        }
+        /// <summary>
+        /// 找到的文字
+        /// </summary>
+        public string Keyword
+        {
+            get { return _keyword; }
+        }
+        /// <summary>
+        /// 初始对象
+        /// </summary>
+        public static DirtyWordModel Empty
+        {
+            get
+            {
+                return new DirtyWordModel(-1, "");
+            }
+        }
+    }
+}
