7c17d222cb60fff02d2cea31d4a1569aecf59f92
Materal <cloomcmx1554@hotmail.com>
2017-12-07 17:50:47 +0800

    修改m-Tools文件合并

--- MateralTools.MWebLib/Index.ts
+++ MateralTools.MWebLib/Index.ts
@@ -1,15 +1,5 @@
 ﻿/// <reference path="lib/m-reset/m-reset.ts" />
-/// <reference path="lib/m-tools/arraymanager.ts" />
-/// <reference path="lib/m-tools/dommanager.ts" />
-/// <reference path="lib/m-tools/encryptionmanager.ts" />
-/// <reference path="lib/m-tools/httpmanager.ts" />
-/// <reference path="lib/m-tools/jsonmanager.ts" />
-/// <reference path="lib/m-tools/localdatamanager.ts" />
-/// <reference path="lib/m-tools/materaltoolsenums.ts" />
-/// <reference path="lib/m-tools/materaltoolsmodels.ts" />
-/// <reference path="lib/m-tools/mathmanager.ts" />
-/// <reference path="lib/m-tools/objectmanager.ts" />
-/// <reference path="lib/m-tools/toolmanager.ts" />
+/// <reference path="lib/m-tools/m-tools.ts" />
 'use strict';
 namespace MateralTools.MWebLib {
     import MDMa = MateralTools.DOMManager;
